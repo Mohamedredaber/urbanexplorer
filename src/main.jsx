efa453281099ba733bfb7ed8512dac62c5266326
mohamed reda <ridabarhouma@gmail.com>
2025-12-20 14:48:58 +0100

    la modifiaction du structure a ete realise avec succes

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { Store } from "./store/Store";
+import { Store } from "./app/Store";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider store ={Store}>
-      <App />
+      <Provider store={Store}>
+        <App />
       </Provider>
     </BrowserRouter>
   </StrictMode>
