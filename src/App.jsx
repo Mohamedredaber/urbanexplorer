4e380d2ae39aeed0bea118f6c398b71bb48cf766
mohamed reda <ridabarhouma@gmail.com>
2025-12-29 01:42:04 +0100

    update home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { Route,Routes } from "react-router-dom";
 import "./App.css";
-
 import Layout from "./components/Layout/Layout";
 import Home from './pages/Home/Home.jsx'
 import NotFound from './pages/NotFound/NotFound.jsx'
-import PlacesPage from "./pages/PlacesPage/PlacesPage.jsx";
+import Explore from "./pages/PlacesPage/Explore.jsx";
 import About from './pages/About/About.jsx'
-import AboutCopy from "./pages/Aboutcopy/AboutCopy.jsx";
 function App() {
   return (
     <>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/lieux" element={<PlacesPage />} />
+          <Route path="/explore" element={<Explore />} />
           <Route path="/about" element={<About />} />
         </Route>
         <Route path="*" element={<NotFound />} />
